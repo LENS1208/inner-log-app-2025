7f5efc0620259d59532caa7fa273fa8db52a61ee
LENS1208 <kan.yamaji@gmail.com>
2025-11-25 17:03:36 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 import "./lib/tokens.css";
 import "./index.css";
 import App from "./App";
-import "./scripts/migrate-demo-data";
+// Temporarily disabled to fix infinite loop issue
+// import "./scripts/migrate-demo-data";
 import { ThemeProvider } from "./lib/theme.context";
 
 if ('scrollRestoration' in history) {
